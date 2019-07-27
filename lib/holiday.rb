d30ecf9d9d8d6c2a05a1f380dac8267a416b0a66
LaNice Powell <lanicemp@aol.com>
2019-07-27 13:51:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
     holidays.each do |holiday_name ,supply_array| 
      if supply_array.include?("BBQ")
-       puts holiday_name
+       
        
    #binding.pry 
    
